0bf8d65216a4cd0cda21c811d4ffd387bc34715b
emilaine <emi.briet.12@hotmail.com>
2022-03-08 15:32:40 -0300

    primeiro commit

---
+++ projeto 2 landing com animação/script.js
@@ -0,0 +1,27 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".animate-inicial-txt", {
+    // duração da animação
+    duration: 0.7,
+    // opacidade que vai começar a animação
+    opacity: 0,
+    // posicção do y que vai começar os elementos na animação
+    y: -150,
+    // tempo entre a animação dos elementos com a mesma classe 
+    stagger: 0.4
+});
+gsap.from(".animate-inicial-img", {
+    duration: 0.9,
+    opacity: 0,
+    x: 250
+});
+gsap.from(".animate-artigos", {
+    duration: 0.8,
+    opacity: 0,
+    x: -150,
+    stagger: 0.4,
+    delay: 1.2
+});
+gsap.from(".animate-final", {
+    
+});
